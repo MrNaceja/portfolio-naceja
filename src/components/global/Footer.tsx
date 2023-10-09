fb9a705104e581f6ad5e68d58f88a4eab10a0b96
Eduardo Toriani <edutoriani13@gmail.com>
2023-10-09 14:12:42 -0300

    Escapado string

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -3,9 +3,9 @@ import { IoMdHeart } from 'react-icons/io'
 export const Footer = () => {
     return (
         <footer className="flex w-full h-14 text-gray-500 items-center justify-center py-2 text-sm gap-1 animate-pulse">
-          Made with <IoMdHeart className='text-sky-500' size={13}/>
+          Made with <IoMdHeart className="text-sky-500" size={13}/>
           by 
-          <strong className='font-semibold'>Eduardo "Naceja" Toriani</strong>
+          <strong className="font-semibold">{`Eduardo "Naceja" Toriani`}</strong>
         </footer>
     )
 }
